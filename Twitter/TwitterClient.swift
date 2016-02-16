b2cc4cef82d59d79ef692b5459933e0d6ba0e84b
Carly Burroughs <GREG@greg-burroughss-macbook.local>
2016-02-16 03:02:45 -0500

    initial commit

--- Twitter/TwitterClient.swift
+++ Twitter/TwitterClient.swift
@@ -7,7 +7,23 @@
 //
 
 import UIKit
+import BDBOAuth1Manager
+
+let twitterConsumerKey = "myHEMVvlCwtwb2zBIFHQ7FAZ8"
+let twitterConsumerSecret = "5fJgodlhGnZlqKaCVoNC8chjsxfhkwFwV1BZhiQ4Uv2SB2bnwN"
+let twitterBaseURL = NSURL(string: "https://api.twitter.com")
+
 
 class TwitterClient: BDBOAuth1SessionManager {
-
+    
+    class var sharedInstance: TwitterClient {
+        struct Static {
+            static let instance = TwitterClient(baseURL: twitterBaseURL, consumerKey: twitterConsumerKey, consumerSecret: twitterConsumerSecret)
+            }
+        return Static.instance
+    }
 }
+    
+
+    
+    
