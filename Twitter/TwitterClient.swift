6ccfd60d2358c61165bcf0b1dfc3c728e48e3501
Carly Burroughs <GREG@greg-burroughss-macbook.local>
2016-02-16 03:02:22 -0500

    initial commit

---
+++ Twitter/TwitterClient.swift
@@ -0,0 +1,13 @@
+//
+//  TwitterClient.swift
+//  Twitter
+//
+//  Created by Carly Burroughs on 2/16/16.
+//  Copyright © 2016 Carly Burroughs. All rights reserved.
+//
+
+import UIKit
+
+class TwitterClient: BDBOAuth1SessionManager {
+
+}
